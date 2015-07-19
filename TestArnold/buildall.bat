e27dd6dd03c6cac5f90df78788000359982cd035
Stephen Taylor <stephen.taylor.dev@gmail.com>
2015-07-19 19:53:14 -0400

    Moved kl2edk folder

--- TestArnold/buildall.bat
+++ TestArnold/buildall.bat
@@ -6,8 +6,8 @@ if not exist GenKL mkdir GenKL
 call python ../GenerateKL.py Fabric2Arnold.cfg.py
 
 REM Generate C++ Files from KL files
-if not exist ./GenCPP/h mkdir ./GenCPP/h
-if not exist ./GenCPP/cpp mkdir ./GenCPP/cpp
-kl2edk GenKL/Fabric2Arnold.fpm.json -o ./GenCPP/h -c ./GenCPP/cpp
+if not exist GenCPP\h mkdir GenCPP\h
+if not exist GenCPP\cpp mkdir GenCPP\cpp
+kl2edk GenKL\Fabric2Arnold.fpm.json -o GenCPP\h -c GenCPP\cpp
 
 popd
